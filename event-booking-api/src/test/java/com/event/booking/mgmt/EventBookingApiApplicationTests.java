2a6a971181a599c0d5ff67001b9c905cb630e5af
Herdi <dfikr94@gmail.com>
2026-02-28 15:39:33 +0700

    first commit

---
+++ event-booking-api/src/test/java/com/event/booking/mgmt/EventBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.booking.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
